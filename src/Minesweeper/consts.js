eb95350703ad03a0e56ae7d992b1f85f893aa829
Kamil Stawik <kamil_stawik@op.pl>
2021-11-03 09:35:11 +0100

    difficultyLevels added to consts

---
+++ src/Minesweeper/consts.js
@@ -0,0 +1,16 @@
+export const difficultyLevels = [
+    {
+        name: "beginner",
+        minesQuantity: 10,
+        fields: 64,
+        columns: 8,
+        rows: 8,
+    },
+    {
+        name: "intermediate",
+        minesQuantity: 40,
+        fields: 256,
+        columns: 16,
+        rows: 16,
+    },
+];
