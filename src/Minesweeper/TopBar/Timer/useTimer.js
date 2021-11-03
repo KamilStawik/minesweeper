5b6c370cd8504484a4114387999a1a187e55c632
Kamil Stawik <kamil_stawik@op.pl>
2021-11-03 09:02:57 +0100

    timer reser added

--- src/Minesweeper/TopBar/Timer/useTimer.js
+++ src/Minesweeper/TopBar/Timer/useTimer.js
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 const useTimer = (gameStatus) => {
 
     const [actualTime, setActualTime] = useState(0);
-    console.log(gameStatus);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
+            gameStatus === "initial" && setActualTime(0);
             gameStatus === "gameIsOn" && setActualTime(PreviousActualTime => PreviousActualTime + 0.1);
         }, 100);
         return () => {
