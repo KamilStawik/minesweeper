cc2d71f655e3ec7d27cc151c30d3021784d4de57
Kamil Stawik <kamil_stawik@op.pl>
2021-11-04 19:51:49 +0100

    new color added to theme

--- src/Minesweeper/styled.js
+++ src/Minesweeper/styled.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+    margin: 100px auto;
+    max-width: 600px;
+    padding: 20px;
+    border-radius: 20px;
+    background-color: ${({ theme }) => theme.colors.mainLight};
     display: flex;
     flex-direction: column;
     align-items: center;
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,7 @@
 export const theme = {
     colors: {
         mainDark: "#1e2832",
+        mainLight: "#c8c8dc",
     },
 
     breakPoints: {
