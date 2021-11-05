424a3890d48b1047b5cc3e72de93e4e909b5c115
Kamil Stawik <kamil_stawik@op.pl>
2021-11-05 09:36:11 +0100

    GridWrapper styles change

--- src/Minesweeper/Grid/styled.js
+++ src/Minesweeper/Grid/styled.js
@@ -4,4 +4,10 @@ export const GridWrapper = styled.div`
     display: grid;
     grid-gap: 2px;
 	grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    padding: 1px;
+    border-radius: 5px;
+    border-top: 2px solid #555555;
+    border-right: 2px solid #dddddd;
+    border-bottom: 2px solid #dddddd;
+    border-left: 2px solid #555555;
 `;
