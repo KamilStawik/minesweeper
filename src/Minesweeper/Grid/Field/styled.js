52a12cbc86a237a43e514dc28fb4cb0ac8e00e75
Kamil Stawik <kamil_stawik@op.pl>
2021-11-06 16:53:38 +0100

    font size set in Field

--- src/Minesweeper/Grid/Field/styled.js
+++ src/Minesweeper/Grid/Field/styled.js
@@ -3,12 +3,13 @@ import styled, { css } from 'styled-components';
 export const StyledField = styled.button`
 	width: 40px;
     height: 40px;
+    font-size: 13px;
+    font-weight: bold;
     background-color: ${({ theme }) => theme.colors.mainLight};
     border-top: 2px solid ${({ theme }) => theme.colors.lightBorder};
     border-right: 2px solid ${({ theme }) => theme.colors.darkBorder};
     border-bottom: 2px solid ${({ theme }) => theme.colors.darkBorder};
     border-left: 2px solid ${({ theme }) => theme.colors.lightBorder};
-    font-weight: bold;
 
     ${({ revealed }) => revealed && css`
         filter: brightness(108%);
