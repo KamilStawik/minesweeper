4b9007ab24ae33b8decb302d2a793f84d81f58e4
Kamil Stawik <kamil_stawik@op.pl>
2021-11-07 21:02:11 +0100

    flags are now disapiring after game

--- src/Minesweeper/Grid/Field/index.js
+++ src/Minesweeper/Grid/Field/index.js
@@ -27,15 +27,15 @@ const Field = ({ id, mine, coordinates, revealed, markedAsMine, surroundingMines
         <StyledField
             mine={mine}
             revealed={revealed}
-            lost={gameStatus==="lost"}
-            won={gameStatus==="won"}
+            lost={gameStatus === "lost"}
+            won={gameStatus === "won"}
             surroundingMines={surroundingMines}
             onClick={gameStatus !== "lost" && gameStatus !== "won" ? clickHandler : undefined}
             onContextMenu={gameStatus !== "lost" && gameStatus !== "won" ? clickHandler : undefined}
         >
             {revealed && mine === false && (surroundingMines === 0 ? "" : surroundingMines)}
             {revealed && mine && "💣"}
-            {markedAsMine && "🚩"}
+            {markedAsMine && gameStatus === "gameIsOn" && "🚩"}
         </StyledField>
     );
 };
