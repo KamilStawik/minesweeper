772bd024b675d93123f62b4b7a10425391a46b2a
Kamil Stawik <kamil_stawik@op.pl>
2021-11-19 16:48:00 +0100

    code cleaning

--- src/Minesweeper/minesweeperSaga.js
+++ src/Minesweeper/minesweeperSaga.js
@@ -8,26 +8,22 @@ function* saveScoreBoardInLocalStorageHandler() {
 }
 
 function* revealSurroundingFieldsHandler({ payload }) {
-
     const grid = yield select(selectGrid);
 
     if (grid[payload].surroundingMines === 0) {
-
         const fieldCoordinates = yield grid[payload].coordinates;
-        yield console.log(fieldCoordinates);
 
+        //how to make some loop for this?
         const targetIndex1 = yield grid.findIndex(field => field.coordinates.column === fieldCoordinates.column - 1 && field.coordinates.row === fieldCoordinates.row - 1);
         yield grid[payload].surroundingMines === 0 && targetIndex1 !== -1 && grid[targetIndex1].revealed === false && (put(leftClick(targetIndex1)));
         const targetIndex2 = yield grid.findIndex(field => field.coordinates.column === fieldCoordinates.column && field.coordinates.row === fieldCoordinates.row - 1);
         yield grid[payload].surroundingMines === 0 && targetIndex2 !== -1 && grid[targetIndex2].revealed === false && (put(leftClick(targetIndex2)));
         const targetIndex3 = yield grid.findIndex(field => field.coordinates.column === fieldCoordinates.column + 1 && field.coordinates.row === fieldCoordinates.row - 1);
         yield grid[payload].surroundingMines === 0 && targetIndex3 !== -1 && grid[targetIndex3].revealed === false && (put(leftClick(targetIndex3)));
-
         const targetIndex4 = yield grid.findIndex(field => field.coordinates.column === fieldCoordinates.column - 1 && field.coordinates.row === fieldCoordinates.row);
         yield grid[payload].surroundingMines === 0 && targetIndex4 !== -1 && grid[targetIndex4].revealed === false && (put(leftClick(targetIndex4)));
         const targetIndex5 = yield grid.findIndex(field => field.coordinates.column === fieldCoordinates.column + 1 && field.coordinates.row === fieldCoordinates.row);
         yield grid[payload].surroundingMines === 0 && targetIndex5 !== -1 && grid[targetIndex5].revealed === false && (put(leftClick(targetIndex5)));
-
         const targetIndex6 = yield grid.findIndex(field => field.coordinates.column === fieldCoordinates.column - 1 && field.coordinates.row === fieldCoordinates.row + 1);
         yield grid[payload].surroundingMines === 0 && targetIndex6 !== -1 && grid[targetIndex6].revealed === false && (put(leftClick(targetIndex6)));
         const targetIndex7 = yield grid.findIndex(field => field.coordinates.column === fieldCoordinates.column && field.coordinates.row === fieldCoordinates.row + 1);
